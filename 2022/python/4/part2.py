be883aa46a137927f6989fed7182eb7a53c78b06
Fran Sanchez <fran.sanchez@five.ai>
2022-12-04 11:35:11 +0000

    [AoC22][Python] Solution day 4

---
+++ 2022/python/4/part2.py
@@ -0,0 +1,29 @@
+def part2(input_filepath="../../data/4/input"):
+    with open(input_filepath) as f:
+        data = f.read().splitlines()
+
+    def get_sections(l):
+        for i in l:
+            s1, s2 = i.split(",")
+            s1 = s1.split("-")
+            s1 = set(range(int(s1[0]), int(s1[1]) + 1))
+            s2 = s2.split("-")
+            s2 = set(range(int(s2[0]), int(s2[1]) + 1))
+            yield s1, s2
+
+    total = 0
+    for sec1, sec2 in get_sections(data):
+        if sec1.intersection(sec2):
+            total += 1
+
+    return total
+
+
+if __name__ == "__main__":
+    import unittest
+
+    X = 4
+    tc = unittest.TestCase()
+    tc.assertEqual(part2("../../data/4/test_input"), X)
+
+    print(f"The result is {part2()}")
